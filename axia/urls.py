56da93dce75fb255059d6563646e958680406fda
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-28 13:33:26 -0300

    alterando varios arquivos

--- axia/urls.py
+++ axia/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("admin/imoveis/", include("imoveis.urls")),
-    path("admin/leiloes/", include("leiloes.urls")),
+    # path("admin/leiloes/", include("leiloes.urls")),
     path("admin/financeiro/", include("financeiro.urls")),
     path("admin/documentos/", include("documentos.urls")),
     # path("admin/custos/", include("custos.urls")),
--- leiloes/apps.py
+++ leiloes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LeiloesConfig(AppConfig):
     name = "leiloes"
     verbose_name = "Leilões"
+    
