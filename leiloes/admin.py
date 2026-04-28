8a687acaaf4ef3c6c375035cc5851476933905cc
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-28 14:14:04 -0300

    alterando varios arquivos

--- leiloes/admin.py
+++ leiloes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Leilao
 
+
 @admin.register(Leilao)
 class LeilaoAdmin(admin.ModelAdmin):
-    list_display = ("imovel", "data_leilao", "valor_inicial", "status")
-    list_filter = ("status",)
-    search_fields = ("imovel__matricula",)
+    list_display = ("processo", "data_leilao", "valor_minimo", "tipo")
+    list_filter = ("tipo", "forma")
+    search_fields = ("processo", "comarca")
--- leiloes/views.py
+++ leiloes/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def leilao_list(request):
-    # Lógica para listar os leilões
-    return HttpResponse("Lista de Leilões")
-
 
 # Create your views here.
