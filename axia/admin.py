6940edd3a42397d19e4e32c1ed335821118afec7
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-28 17:11:05 -0300

    alterando varios arquivos

--- axia/admin.py
+++ axia/admin.py
@@ -1,12 +1,12 @@
-from django.contrib.admin import AdminSite
-from django.db.models import Sum
-from imoveis.models import Imovel
+# from django.contrib.admin import AdminSite
+# from django.db.models import Sum
+# from imoveis.models import Imovel
 
 
-class MyAdminSite(AdminSite):
-    index_template = "admin/index.html"
+# class MyAdminSite(AdminSite):
+#     index_template = "admin/index.html"
 
-    def each_context(self, request):
-        context = super().each_context(request)
-        context["total_imoveis"] = Imovel.objects.count()
-        return context
+#     def each_context(self, request):
+#         context = super().each_context(request)
+#         context["total_imoveis"] = Imovel.objects.count()
+#         return context
