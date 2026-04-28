68f416c2f4524536a5fc78d91998ad7e838c1593
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-28 10:24:35 -0300

    alteraçao em varios arquivos

--- leiloes/admin.py
+++ leiloes/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Leilao
 
-admin.site.register(Leilao)
+
+@admin.register(Leilao)
+class LeilaoAdmin(admin.ModelAdmin):
+    list_display = ("imovel", "data_leilao", "valor_inicial", "status")
+    list_filter = ("status",)
+    search_fields = ("imovel__matricula",)
--- users/models.py
+++ users/models.py
@@ -5,5 +5,7 @@ class User(AbstractUser):
     TIPO_CHOICES = (
         ('socio', 'Sócio'),
         ('analista', 'Analista'),
+        ('gestor', 'Gestor'),
+        ('administrador', 'Administrador'),
     )
     tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
