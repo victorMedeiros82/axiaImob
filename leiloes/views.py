00fa1c5574eb11849fc9846d134499c83170722a
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-28 12:27:15 -0300

    alterando varios arquivos

--- leiloes/views.py
+++ leiloes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def leilao_list(request):
     # Lógica para listar os leilões
-    return render(request, "leiloes/leilao_list.html")
+    return HttpResponse("Lista de Leilões")
 
 
 # Create your views here.
