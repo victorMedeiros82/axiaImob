5a023affed58ba34a7c396ad9d794bd58554f469
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-28 12:46:00 -0300

    alterando varios arquivos

--- leiloes/urls.py
+++ leiloes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.leilao_list, name="leilao_list"),
+    path("leilao/", views.leilao_list, name="leilao_list"),
 ]
