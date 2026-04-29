927ef0c5a7ea6d296a4beb06c7a97077f899eacd
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-29 00:12:00 -0300

    alterando varios arquivos

--- axia/urls.py
+++ axia/urls.py
@@ -5,18 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("admin/imoveis/", include("imoveis.urls")),
-    path("admin/leiloes/", include("leiloes.urls")),
-    path("admin/financeiro/", include("financeiro.urls")),
-    path("admin/documentos/", include("documentos.urls")),
-    path("admin/custos/", include("custos.urls")),
-    path("admin/relatorios/", include("relatorios.urls")),
     path("", include("core.urls")),
-    # path("admin/usuarios/", include("usuarios.urls")),
-    # path("admin/configuracoes/", include("configuracoes.urls")),
-    # path("admin/ajuda/", include("ajuda.urls")),
-    # path("admin/suporte/", include("suporte.urls")),
-    # path("admin/feedback/", include("feedback.urls")),
 ]
 
 # Servir arquivos de mídia em desenvolvimento
