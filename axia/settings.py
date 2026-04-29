4ccccc71b558b5e2f953ede4bb880e137dcfa102
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-29 13:54:25 -0300

    alterando varios arquivos

--- axia/settings.py
+++ axia/settings.py
@@ -10,7 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(#nr0(6kvpoa$z10izxi09@-+d!7k71$&-k8*vo^#=sbwo6fb*"
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-(#nr0(6kvpoa$z10izxi09@-+d!7k71$&-k8*vo^#=sbwo6fb*"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
--- financeiro/models.py
+++ financeiro/models.py
@@ -6,15 +6,17 @@ from imoveis.models import (
 )  # Importa o modelo Imovel para criar relacionamentos com o modelo Faturamento
 
 
-class Faturamento(models.Model): # Define o modelo Faturamento, que representa os registros de faturamento relacionados a imóveis
+class Faturamento(
+    models.Model
+):  # Define o modelo Faturamento, que representa os registros de faturamento relacionados a imóveis
     imovel = models.ForeignKey(Imovel, on_delete=models.CASCADE)
     TIPO = [
         ("venda", "Venda"),
-        ("locação", "Locação"),
+        ("locacao", "Locação"),
     ]
     tipo = models.CharField(max_length=20, choices=TIPO, default="venda")
     valor = models.DecimalField(max_digits=12, decimal_places=2)
     data = models.DateField()
 
     def __str__(self):
-        return self.tipo
+        return f"{self.get_tipo_display()} - {self.imovel} - R$ {self.valor}"
