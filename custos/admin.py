4edeb394bd7a4781a03b29a93b65a1fec78fbd4c
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-29 00:45:15 -0300

    alterando varios arquivos

--- custos/admin.py
+++ custos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustoAquisicao
+from .models import CustoAquisicao, CustoReforma, CustoHolding
 
 
 @admin.register(CustoAquisicao)
@@ -9,4 +9,21 @@ class CustoAquisicaoAdmin(admin.ModelAdmin):
     search_fields = ("imovel__matricula",)
 
 
-# admin.site.register(CustoAquisicao, CustoAquisicaoAdmin)
+@admin.register(CustoReforma)
+class CustoReformaAdmin(admin.ModelAdmin):
+    list_display = (
+        "imovel",
+        "descricao",
+        "valor_previsto",
+        "valor_real",
+        "data_inicio",
+    )
+    list_filter = ("data_inicio", "data_fim")
+    search_fields = ("imovel__matricula", "descricao")
+
+
+@admin.register(CustoHolding)
+class CustoHoldingAdmin(admin.ModelAdmin):
+    list_display = ("imovel", "tipo", "valor_mensal", "data_inicio", "data_fim")
+    list_filter = ("tipo", "data_inicio")
+    search_fields = ("imovel__matricula",)
