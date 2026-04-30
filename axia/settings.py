f278af4a4d0e2c4bfeeeef8c8496baabc1e833a6
victorMedeiros82 <victor.medeiros82@gmail.com>
2026-04-30 00:48:11 -0300

    alterando varios arquivos

--- axia/settings.py
+++ axia/settings.py
@@ -129,6 +129,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
